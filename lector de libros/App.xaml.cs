5549ec741b9fd8868311795fa1c229bf41cc7563
JC <javic87@gmail.com>
2026-08-04 21:33:58 +0200

    Scaffold inicial del proyecto WPF

---
+++ lector de libros/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace lector_de_libros
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
